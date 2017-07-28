850d2c16cdc096ffac2fa80549fee7d395265c87
afok <foktmalexander@gmail.com>
2017-07-28 10:57:21 -0400

    updating dao's and data flow diagram

--- src/dao/ApproverDao.java
+++ src/dao/ApproverDao.java
@@ -2,29 +2,32 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 
+import jsonString.JSONConvert;
 import util.ConnectionFactory;
 
-public class ApproverDao {
+public abstract class ApproverDao {
 Connection conn;
 int rfId;
+int eId;
 
-	public ApproverDao(int rfId) {
+	public ApproverDao(int rfId, int eId) {
 		this.rfId = rfId;
+		this.eId = eId;
 	}
 	
-	public boolean approveReimbursement(int status) throws SQLException{
+	public boolean approveReimbursement() throws SQLException{
 		conn = ConnectionFactory.getInstance().getConnection();
 		conn.setAutoCommit(false);
-		String sql = "update Reimbursement_Form set status = ? where rf_id = ?";
+		String sql = "update Reimbursement_Form set status = status + 1 where rf_id = ?";
 		
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setInt(1, status);
-		stmt.setInt(2, rfId);
-		int count = stmt.executeUpdate();
+		stmt.setInt(1, this.rfId);
 		Savepoint s = conn.setSavepoint();
+		int count = stmt.executeUpdate();
 		
 		if (count != 1) {
 			conn.rollback(s);
@@ -41,9 +44,9 @@ int rfId;
 		String sql = "update Reimbursement_Form set status = 0 where rf_Id = ?";
 		
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setInt(1, rfId);
-		int count = stmt.executeUpdate();
+		stmt.setInt(1, this.rfId);
 		Savepoint s = conn.setSavepoint();
+		int count = stmt.executeUpdate();
 		
 		if (count!=1) {
 			conn.rollback(s);
@@ -54,22 +57,84 @@ int rfId;
 		return count != 1;
 	}
 	
-	public boolean changeAmount(double amount) throws SQLException {
+	public String[] getRFInfo(int rfStatus) throws SQLException {
 		conn = ConnectionFactory.getInstance().getConnection();
-		conn.setAutoCommit(false);
-		String sql = "update Reimbursement_form set cost = ? where rfId = ?";
+		String sql = "select * from Reimburse_Form where status = ?";
 		
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setDouble(1, amount);
-		int count = stmt.executeUpdate();
-		Savepoint s = conn.setSavepoint();
+		stmt.setInt(1, rfStatus);
+		ResultSet rs = stmt.executeQuery();
 		
-		if (count!=1) {
-			conn.rollback(s);
+		String[] returnArray = new String[rs.getFetchSize()];
+		int i = 0;
+		while (rs.next()){
+			
+			String[] strArray = new String[13];
+			strArray[0] = String.valueOf(rs.getInt(1));	 	// rf_id
+			strArray[1] = String.valueOf(rs.getInt(2)); 	// e_id
+			strArray[2] = String.valueOf(rs.getDate(3));	// start_date
+			strArray[3] = String.valueOf(rs.getDate(4));	// start_time
+			strArray[4] = String.valueOf(rs.getDate(5));	// end_time
+			strArray[5] = rs.getString(6);					// location
+			strArray[6] = rs.getString(7);					// description
+			strArray[7] = String.valueOf(rs.getInt(8)); 	// cost
+			strArray[8] = rs.getString(9);					// grading_format
+			strArray[9] = rs.getString(10);					// event_type
+			strArray[10] = rs.getString(11);				// work_related
+			strArray[11] = String.valueOf(rs.getInt(13));	// status
+			strArray[12] = String.valueOf(rs.getDate(14));	// last activity
+			//NOTE: blob object: approval object not included
+			
+			returnArray[i] = JSONConvert.getJSON(strArray);
 		}
-		conn.setAutoCommit(true);
-		conn.close();
-		return count != 1;
+		return returnArray;
+	}
+	
+	public boolean requestForInfo(String detail, int receiver) throws SQLException {
+		conn = ConnectionFactory.getInstance().getConnection();
+		String sql = "select * from Request where rf_id = ? and sender = ? and receiver = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, this.rfId);
+		stmt.setInt(2, this.eId);
+		stmt.setInt(3, receiver);
 		
+		ResultSet rs = stmt.executeQuery();
+		
+		int count;
+		if (!rs.next()){
+			rs.close();
+			stmt.close();
+			
+			sql = "insert into Request(rf_id, sender, receiver, detail) values (?, ?, ?, ?)";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, this.rfId);
+			stmt.setInt(2, this.eId);
+			stmt.setInt(3, receiver);
+			stmt.setString(4, detail);
+			
+			Savepoint s = conn.setSavepoint();
+			count = stmt.executeUpdate();
+			
+			if (count != 1) {
+				conn.rollback(s);
+			}
+		}
+		else {
+			rs.close();
+			stmt.close();
+			
+			sql = "update Request set detail = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, detail);
+			
+			Savepoint s = conn.setSavepoint();
+			count = stmt.executeUpdate();
+			
+			if (count != 1) {
+				conn.rollback(s);
+			}
+		}
+		conn.close();
+		return count == 1;
 	}
 }
---
+++ src/jsonString/JSONConvert.java
@@ -0,0 +1,7 @@
+package jsonString;
+
+public class JSONConvert {
+	public static String getJSON(String[] args) {
+		return "";
+	}
+}
