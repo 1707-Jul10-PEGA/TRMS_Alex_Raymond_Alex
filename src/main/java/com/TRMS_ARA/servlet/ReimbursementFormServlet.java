41e994ef76f75e7f6ecc2cefc631ecabf2328e84
Raymond Bejarano <raymondb522@gmail.com>
2017-07-29 16:29:30 -0400

    working on servlet

---
+++ src/main/java/com/TRMS_ARA/servlet/ReimbursementFormServlet.java
@@ -0,0 +1,29 @@
+package com.TRMS_ARA.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ReimbursementFormServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3457214631478156496L;
+	
+	public ReimbursementFormServlet() {
+		super();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+	}
+	
+}
--- src/main/webapp/ReimbursementForm.js
+++ src/main/webapp/ReimbursementForm.js
@@ -2,3 +2,41 @@ $(document).ready(function() {
     $("#startDay").datepicker();
 });
 
+window.onload = function(){
+	
+	document.getElementById("submit").addEventListener("click", postForm, false);
+	
+}
+
+function postForm() {
+	
+	var head = document.getElementById("head");
+	
+	var xhr = new XMLHttpRequest();
+	
+	xhr.onreadystatechange = function(){
+	
+		switch (xhr.readyState) {
+		
+		case 0:
+			
+			break;
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		}
+	}
+	
+	
+	
+	
+}
