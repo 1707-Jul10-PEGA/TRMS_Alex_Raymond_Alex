48bb59bca69009ab62c76ca05c0d0fd8999b3e46
Raymond Bejarano <raymondb522@gmail.com>
2017-07-31 21:09:55 -0400

    progress is being made

--- src/main/java/com/TRMS_ARA/servlet/GetOtherFormServlet.java
+++ src/main/java/com/TRMS_ARA/servlet/GetOtherFormServlet.java
@@ -1,5 +1,24 @@
 package com.TRMS_ARA.servlet;
 
-public class GetOtherFormServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetOtherFormServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5076656763328481515L;
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+	}
+	
+	
 }
